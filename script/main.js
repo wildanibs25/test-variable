d20a3bd6b5a1a18dd0f466c6a6f8a024e60f09cc
wildanibs25 <sin990325@gmail.com>
2024-11-15 23:46:28 +0700

    update js

--- script/main.js
+++ script/main.js
@@ -216,7 +216,7 @@ const animationTimeline = () => {
       "+=1"
     )
     .from(".idea-1", 0.7, ideaTextTrans)
-    .to(".idea-1", 0.7, ideaTextTransLeave, "+=2.5")
+    .to(".idea-1", 2, ideaTextTransLeave, "+=2.5")
     .from(".idea-2", 0.7, ideaTextTrans)
     .to(".idea-2", 0.7, ideaTextTransLeave, "+=2.5")
     .from(".idea-3", 0.7, ideaTextTrans)
